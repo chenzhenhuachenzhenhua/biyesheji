f172cdeba667b89a3999c5bb46fe8d7ea2c29780
czh <mav2015guo@gmail.com>
2021-05-02 22:32:43 +0800

    提交内容：布局调整 提交人：zhenhua.chen@77hub.com

--- src/pages/dashboard/analysis/components/SalesCard.tsx
+++ src/pages/dashboard/analysis/components/SalesCard.tsx
@@ -175,13 +175,37 @@ const SalesCard = ({
             </Col> */}
           </Row>
           <Row>
-  
-            <Col xl={8} lg={12} md={12} sm={24} xs={24}>
+            <Col xl={12} lg={12} md={12} sm={12} xs={12}>
               <div className={styles.salesRank}>
                 <h4 className={styles.rankingTitle}>
                   <FormattedMessage
                     id="dashboardandanalysis.analysis.sales-ranking"
                     defaultMessage="Sales Ranking"
+                  />
+                </h4>
+                <ul className={styles.rankingList}>
+                  {rankingListData.map((item, i) => (
+                    <li key={item.title}>
+                      <span className={`${styles.rankingItemNumber} ${i < 3 ? styles.active : ''}`}>
+                        {i + 1}
+                      </span>
+                      <span className={styles.rankingItemTitle} title={item.title}>
+                        {item.title}
+                      </span>
+                      <span className={styles.rankingItemValue}>
+                        {numeral(item.total).format('0,0')}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Col>
+            <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+              <div className={styles.salesRank}>
+                <h4 className={styles.rankingTitle}>
+                  <FormattedMessage
+                    id="dashboardandanalysis.analysis.visits-ranking"
+                    defaultMessage="工资排名"
                   />
                 </h4>
                 <ul className={styles.rankingList}>
@@ -212,6 +236,45 @@ const SalesCard = ({
           }
           key="views"
         >
+          <Row>
+            <Col xl={16} lg={12} md={12} sm={24} xs={24}>
+              <div className={styles.salesBar}>
+                <Bar
+                  height={292}
+                  title={
+                    <FormattedMessage
+                      id="dashboardandanalysis.analysis.visits-trend"
+                      defaultMessage="Visits Trend"
+                    />
+                  }
+                  data={salesData}
+                />
+              </div>
+            </Col>
+            <Col xl={8} lg={12} md={12} sm={24} xs={24}>
+              <div className={styles.salesRank}>
+                <h4 className={styles.rankingTitle}>
+                  <FormattedMessage
+                    id="dashboardandanalysis.analysis.visits-ranking"
+                    defaultMessage="Visits Ranking"
+                  />
+                </h4>
+                <ul className={styles.rankingList}>
+                  {rankingListData.map((item, i) => (
+                    <li key={item.title}>
+                      <span className={`${styles.rankingItemNumber} ${i < 3 ? styles.active : ''}`}>
+                        {i + 1}
+                      </span>
+                      <span className={styles.rankingItemTitle} title={item.title}>
+                        {item.title}
+                      </span>
+                      <span>{numeral(item.total).format('0,0')}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </Col>
+          </Row>
           <Row>
             <Col xl={16} lg={12} md={12} sm={24} xs={24}>
               <div className={styles.salesBar}>
