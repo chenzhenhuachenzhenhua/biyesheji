5f7d0db6e5ef439c0344c33807a4931007496cd1
czh <mav2015guo@gmail.com>
2021-05-08 11:18:50 +0800

    提交内容：热门搜索 提交人：zhenhua.chen@77hub.com

--- config/config.ts
+++ config/config.ts
@@ -86,12 +86,12 @@ export default defineConfig({
                   path: '/dashboard/analysis',
                   component: './dashboard/analysis',
                 },
-                // {
-                //   name: 'monitor',
-                //   icon: 'smile',
-                //   path: '/dashboard/monitor',
-                //   component: './dashboard/monitor',
-                // },
+                {
+                  name: 'monitor',
+                  icon: 'smile',
+                  path: '/dashboard/monitor',
+                  component: './dashboard/monitor',
+                },
                 // {
                 //   name: 'workplace',
                 //   icon: 'smile',
